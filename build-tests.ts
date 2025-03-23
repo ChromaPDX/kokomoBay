11e972fa6b54c049933b47f2ff8141c235911358
adam wong <adamwong246@gmail.com>
2025-03-23 11:44:32 -0700

    0.89.0

--- build-tests.ts
+++
@@ -1,5 +0,0 @@
-import { ITProject } from "testeranto/src/Project";
-
-import("./testeranto.mjs").then((module) => {
-  new ITProject(module.default);
-});
--- run-tests.ts
+++
@@ -1,5 +0,0 @@
-import Puppeteer from "testeranto/Puppeteer";
-
-import("./testeranto.mjs").then((module) => {
-  Puppeteer(module.default);
-});
